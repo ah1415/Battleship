0fbf1739231e9b556bbc6c72efefce7e747c4b0c
Austin <austin.austin.no1@gmail.com>
2019-10-24 20:23:13 -0400

    Add files via upload

---
+++ images.s
@@ -0,0 +1,32 @@
+.global BATTLESHIP
+BATTLESHIP: .incbin "D:/Project/battleship.bin"
+
+.global LOSE
+LOSE: .incbin "D:/Project/youlose.bin"
+
+.global WIN
+WIN: .incbin "D:/Project/youwin.bin"
+
+.global MODE
+MODE: .incbin "D:/Project/mode.bin"
+
+.global BOTSHIP
+BOTSHIP: .incbin "D:/Project/botship.bin"
+
+.global PSHOT
+PSHOT: .incbin "D:/Project/pshot.bin"
+
+.global PSINK
+PSINK: .incbin "D:/Project/psink.bin"
+
+.global PMISS
+PMISS: .incbin "D:/Project/pmiss.bin"
+
+.global CSHOT
+CSHOT: .incbin "D:/Project/cshot.bin"
+
+.global CSINK
+CSINK: .incbin "D:/Project/csink.bin"
+
+.global CMISS
+CMISS: .incbin "D:/Project/cmiss.bin"
